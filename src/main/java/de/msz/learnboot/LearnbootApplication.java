98ead8d66a25b8ccd6ad0bcabda7787c1e96f3ac
Mathias Schulze <mathias-schulze@web.de>
2021-01-18 22:07:42 +0100

    changed mapping of service to '/' to test deploy via GitHub

--- src/main/java/de/msz/learnboot/LearnbootApplication.java
+++ src/main/java/de/msz/learnboot/LearnbootApplication.java
@@ -14,7 +14,7 @@ public class LearnbootApplication {
 		SpringApplication.run(LearnbootApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
