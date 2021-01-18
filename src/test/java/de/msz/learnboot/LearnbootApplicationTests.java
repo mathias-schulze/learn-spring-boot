eab160300241627fd0daaebfd8d5146eccfda0bf
Mathias Schulze <mathias-schulze@web.de>
2021-01-18 21:13:06 +0100

    spring initializr

---
+++ src/test/java/de/msz/learnboot/LearnbootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.msz.learnboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
